eb1a37b892945a27c5d1459697daf88b85e90909
sabrinavohra <sabrina.vohra@menloschool.org>
2024-01-12 13:44:19 -0800

    Created program

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,26 @@ public class Person {
     private String firstName;
     private String lastName;
     private String phoneNumber;
+
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber =  phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String toString() {
+        return this.firstName + " " + this.lastName + " - #" + this.phoneNumber;
+    }
 }
